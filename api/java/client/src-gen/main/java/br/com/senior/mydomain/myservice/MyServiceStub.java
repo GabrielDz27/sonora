6356f66d8bc7edb7dd4b224edd14e852ed6096e4
gabriel ferreira <gabriel.ferreira@senior.com.br>
2026-02-19 09:57:04 -0300

    init project api

---
+++ api/java/client/src-gen/main/java/br/com/senior/mydomain/myservice/MyServiceStub.java
@@ -0,0 +1,112 @@
+/**
+ * This is a generated file. DO NOT EDIT ANY CODE HERE, YOUR CHANGES WILL BE LOST.
+ */
+package br.com.senior.mydomain.myservice;
+
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+* 
+*/
+public interface MyServiceStub {
+
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * 
+	 * Chamada síncrona, o valor de timeout deve ser informado em ms 
+	 */
+	HelloWorldOutput helloWorld(HelloWorldInput input, long timeout);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * 
+	 * Chamada assíncrona
+	 */
+	void helloWorld(HelloWorldInput input);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * 
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<HelloWorldOutput> helloWorldRequest(HelloWorldInput input);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * 
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<HelloWorldOutput> helloWorldRequest(HelloWorldInput input, long timeout);
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Default 'getMetadata' query. Every service must handle this command and return metadata in the format requested.
+	 * Chamada síncrona, o valor de timeout deve ser informado em ms 
+	 */
+	GetMetadataOutput getMetadata(GetMetadataInput input, long timeout);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Default 'getMetadata' query. Every service must handle this command and return metadata in the format requested.
+	 * Chamada assíncrona
+	 */
+	void getMetadata(GetMetadataInput input);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Default 'getMetadata' query. Every service must handle this command and return metadata in the format requested.
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<GetMetadataOutput> getMetadataRequest(GetMetadataInput input);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Default 'getMetadata' query. Every service must handle this command and return metadata in the format requested.
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<GetMetadataOutput> getMetadataRequest(GetMetadataInput input, long timeout);
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Returns a list with all dependencies from this service, along with their respective versions
+	 * Chamada síncrona, o valor de timeout deve ser informado em ms 
+	 */
+	GetDependenciesOutput getDependencies(long timeout);
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Returns a list with all dependencies from this service, along with their respective versions
+	 * Chamada assíncrona
+	 */
+	void getDependencies();
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Returns a list with all dependencies from this service, along with their respective versions
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<GetDependenciesOutput> getDependenciesRequest();
+	
+	/**
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 * Returns a list with all dependencies from this service, along with their respective versions
+	 * Chamada assíncrona utilizando request
+	 */
+	CompletableFuture<GetDependenciesOutput> getDependenciesRequest(long timeout);
+
+
+	/**
+	 * Chamada assíncrona para o método publishServiceStarted 
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 */
+	void publishServiceStarted( ServiceStartedPayload input );
+			
+	
+	/**
+	 * Chamada assíncrona para o método publishNotifyUserEvent 
+	 * Warning: this operation is PRIVATE and may have its behavior changed at any time without notice
+	 */
+	void publishNotifyUserEvent( NotifyUserEventPayload input );
+			
+	
+
+}
